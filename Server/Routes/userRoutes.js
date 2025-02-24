d6cbc9c01a5aeb30d60fca42428bc6af31f78515
RamHiren <ramhiren25@gmail.com>
2025-02-24 11:05:20 +0530

    SignUp-Login Backend

---
+++ Server/Routes/userRoutes.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../Model/user');
+const { generateToken ,jwtAuthMiddleware } = require('../jwt');
+
+
+router.post('/signup' ,async(req,res)=>{
+    try{
+        const data = req.body;
+        const newUser = new User(data);
+        const savedUser = await newUser.save()
+        console.log("Data saved Successfully");
+        // aasign jwt token
+        const payload ={
+            id : savedUser.id,
+            username : savedUser.username
+        }
+
+        const token = generateToken(payload);
+        console.log("token :" ,token);
+        res.status(200).json({savedUser: savedUser ,token: token});
+
+    }catch(err){
+        console.error(err);
+        // res.status(500).send('Server Error');
+        res.status(500).json({error : 'Internal Server Error'});
+        return;
+    }
+})
+
+router.post('/login', async(req,res)=>{
+    try{
+        const {username ,password} = req.body;
+        const user = await User.findOne({username});
+        if(!user || !(user.password === password)){
+            return res.status(401).json({error : 'Invalid Credentials'});
+        }
+        //genarate token
+        const payload ={
+            id : user.id,
+            username : user.username
+        }
+        const token = generateToken(payload);
+        res.status(200).json({token: token});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({error : 'Internal Server Error'});
+        return;
+    }
+});
+
+
+
+
+
+module.exports = router;
--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const db = require('./db');
+const cors = require('cors');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+app.use(cors());
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+const userRoutes = require('./Routes/userRoutes');
+app.use('/user', userRoutes);
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
